40db93c700744a417f749ae037066aaf9170b205
hoanghiepnb <hiepbh195@gmail.com>
2016-12-03 02:08:17 +0800

    edit

--- a.py
+++ a.py
@@ -1,17 +1,4 @@
-import sys
-import pygame
-pygame.init()
+import subprocess
 
-mov_name = "video.mpg"
-pygame.mouse.set_visible(False)
-pygame.mixer.quit()
-screen = pygame.display.set_mode((320, 240))
-video = pygame.movie.Movie(mov_name)
-screen = pygame.display.set_mode(video.get_size())
-video.play()
-
-while video.get_busy():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            break
-
+while (1) :
+    subprocess.call(["omxplayer", "sample.mp4"])
--- mp3.py
+++ mp3.py
@@ -1,6 +1,3 @@
-import pygame, time
-pygame.init()
-pygame.mixer.music.load('1.mp3')
-pygame.mixer.music.play()
-time.sleep(5)
-pygame.mixer.music.fadeout(5)
+import subprocess
+
+subprocess.call(["omxplayer", "1.mp3"])
