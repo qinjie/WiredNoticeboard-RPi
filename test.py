5f895085c57cce17ea6bb15f23933edd0c556ff6
Hoang Hiep <hiepbh195@gmail.com>
2017-03-01 10:30:35 +0800

    first

---
+++ test.py
@@ -0,0 +1,14 @@
+class Video(object):
+    def __init__(self,path):
+        self.path = path
+
+    def play(self):
+        from os import startfile
+        startfile(self.path)
+
+class Movie_MP4(Video):
+    type = "MP4"
+
+movie = Movie_MP4(r"sample.mp4")
+if __name__ == '__main__':
+    movie.play()
