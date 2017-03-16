d063c88b2cff1c3ed53b186b3b4452e6f918ced4
hoanghiepnb <hiepbh195@gmail.com>
2017-03-16 14:31:15 +0800

    notice board

--- download.py
+++ download.py
@@ -1,4 +1,6 @@
 import urllib.request
-mp3file = urllib.request .urlopen("http://techslides.com/demos/samples/sample.mp4")
-with open('sample.mp4','wb') as output:
+
+# urllib.request.urlretrieve("http://128.199.93.67/WiredNoticeboard-Web/frontend/web/uploads/2_58c0aa25d1f82.mp4", "a.mp4")
+mp3file = urllib.request .urlopen("http://128.199.93.67/WiredNoticeboard-Web/frontend/web/uploads/2_58c0aa25d1f82.mp4")
+with open('b.mp4','wb') as output:
     output.write(mp3file.read())
